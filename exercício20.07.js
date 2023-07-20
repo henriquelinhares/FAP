4e667ff09b97cebdd8023786cefc7a16006fc69d
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-07-20 13:20:19 +0000

    Exercício para o dia 20-07

--- exercício20.07.js
+++ exercício20.07.js
@@ -1,4 +1,24 @@
-var num1 = 7;
-var num2 = 7;
-var resultado = num1 + num2;
-console.log(resultado);
+var minhaVar = 
+minhaVar = 10;
+var soma = 15 + 8;
+soma += 1; 
+soma *= 3;
+console.log(soma);
+//Valor de soma até aqui é 72.
+
+var fazSol = true;
+var comida = ['arroz','feijao','ovo'];
+console.log(comida[1]);
+var igual = soma==minhaVar;
+console.log(igual);
+var menorOu = minhaVar <= soma;
+console.log(menorOu);
+
+function divisao(10, 5){
+    return 
+}
+
+
+
+
+
