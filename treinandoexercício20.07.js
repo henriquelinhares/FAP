65d45f85fdc82f946eb966a16a3324d5c930796d
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-07-20 18:14:20 +0000

    Treinandoexercicio20.07

---
+++ treinandoexercício20.07.js
@@ -0,0 +1,20 @@
+var minhaVar = 
+minhaVar = 10; 
+var soma = 15 + 8;
+soma += 1;
+soma *= 3; 
+console.log(soma);
+
+var fazSol = true;
+var comida = ["arroz", "feijao", "ovo"]
+console.log(comida[1]);
+
+var igual = soma == minhaVar;
+console.log(igual);
+var menorIgual = minhaVar <= soma; 
+console.log(menorIgual);
+
+function divisao(numero1, numero2){
+    return numero1 / numero2;
+}
+console.log(divisao(1,2));
