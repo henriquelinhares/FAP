9e20c40ee0a45240cdf605998aca6f59f4791141
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-07-20 20:01:19 +0000

    Exercício estrutura de decisão

---
+++ exercícioestruturadedecisao.js
@@ -0,0 +1,51 @@
+var numero = 11;
+
+    if(numero > 10){
+    console.log("Este número é maior que 10.");
+    } else{
+    console.log("Este número é menor que 10.");
+    }
+
+var numero2 = 5;
+
+    if(numero2 >= 0){
+        console.log("Este número é Positivo.");
+    } else {
+        console.log("Este número é negativo.");
+    }
+
+var numeroPar = 3;
+
+    if(numeroPar/2 == 0){
+        console.log("Este número é par.");
+    } else{
+        console.log("Este número é impar.");
+    }
+    
+var boleana = 5;
+
+    if(boleana == true || false){
+        console.log("Está variável é boleana.")
+    } else{
+        console.log("Está variável não é boleana.");
+    } 
+    //Rever isso aqui, pela fé kkkkk 
+
+
+var numeroCategorias = 5;
+
+    if(numeroCategorias <= 0){
+        console.log("Este número é negativo.");
+    } else if(numeroCategorias == 0){
+        console.log("Este número é neutro.");
+    } else{
+        console.log("Este número é positivo.");
+    }
+
+var numeroDivisivel = ;
+
+    if(numeroDivisivel == ){
+        console.log("Este número é divisivel por 2.");
+    } 
+
+    //Continuar daqui
