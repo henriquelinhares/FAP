ce054f1ee8632bb2becf2e4c779cfd445b52e01a
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-07-25 18:08:07 +0000

    Exercício 001

---
+++ exercíciocondições.ifelse.js
@@ -0,0 +1,61 @@
+//Exercício sobre estrutura condicionais da plataforma softex. 
+
+const entrada= require('readline-sync');
+
+let nome; 
+    nome = entrada.question("Digite o seu nome: ");
+    console.log(`Olá, ${nome}. Bem vindo!`);
+
+let transporte;
+    transporte = entrada.question(`${nome}, seu meio de transporte é terrestre? `);
+    
+    if(transporte == "sim" || transporte == "Sim"){
+        console.log("Certo. Vamos continuar.");  
+    } else if(transporte == "não" || transporte == "Não"){
+        console.log("Certo. Nossa pesquisa encerra aqui.");
+    } else{
+        console.log("Resposta incorreta.")
+    }
+
+let capacete;
+    capacete = entrada.question("Seu veiculo usa capacete? ")
+
+    if(capacete == "sim" || capacete == "Sim"){
+        console.log(`Certo, ${nome}! Seu veiculo é uma moto.`)
+    } else if(capacete == "não" || capacete == "Não"){
+        console.log("Certo, vamos continuar.");
+    } else{
+        console.log("Resposta incorreta.");
+    }
+
+let umaPessoa; 
+    umaPessoa = entrada.question("Seu veículo cabe apenas uma pessoa? ")
+
+    if(umaPessoa == "sim" || umaPessoa == "Sim"){
+        console.log("Certo, vamos continuar.")
+    } else if(umaPessoa == "não" || umaPessoa == "Não"){
+        console.log(`Obrigado, ${nome}. Nossa pesquisa termina aqui.`);
+    } else {
+        console.log("Resposta incorreta.");
+    }
+
+let pesado;
+    pesado = entrada.question("Seu veículo é pesado? ")
+
+    if(pesado == "sim" || pesado == "Sim"){
+        console.log(`Certo, ${nome}. Seu veiculo é um trator.`);
+    } else if(pesado == "não" || pesado == "Não"){
+        console.log("Certo. Vamos continuar.")
+    } else{
+        console.log("Resposta incorreta.");
+    } 
+
+let biccleta;
+    bicicleta = entrada.question("Seu veiculo usa pedal? ")
+
+    if
+
+    
+    
+    console.log(`Obrigado por responder nossa pesquisa, ${nome}. Até logo.`);
+   
