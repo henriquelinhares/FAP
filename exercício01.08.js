c8c55d9135ceba97ef3ce950f025339f54bc78fb
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-01 18:41:15 +0000

    exercício 01.08

---
+++ exercício01.08.js
@@ -0,0 +1,29 @@
+/* Arrays. 
+    Arrays é um tipo de estrutura de dados. 
+*/
+
+/*let sorvetes = ["morango", "uva", "baunilha", "piatache", "manga", "flocos"]; 
+    sorvetes[sorvetes.length] = "chocolate"
+    sorvetes.splice(3,1);
+    delete sorvetes[sorvetes.length - 1];
+
+console.log(sorvetes.splice(3,1));
+console.log(sorvetes);*/
+
+const meuArray = [10, 20, 30, 40, 50];
+let segundoElemneto = meuArray[2];
+
+console.log(segundoElemneto);
+
+const frutas = ["maçã", "banana", "laranja", "uva", "manga"];
+console.log(frutas.length);
+
+frutas[frutas.length] = "pera";
+frutas.splice(0,1); 
+
+console.log(frutas);
+
+
+const numeros = [10, 20, 30, 40, 50]; 
+
+console.log(numeros.indexOf(25)); 
