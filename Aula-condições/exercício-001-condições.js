2cfb367af88f19f2c83f4d8301db927c061116d5
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-04 21:18:33 +0000

    exercício 001

---
+++ Aula-condições/exercício-001-condições.js
@@ -0,0 +1,8 @@
+/*Condições. 
+A estrutura condicional permite avaliar uma condição e, a partir dela, executar diferentes linhas de 
+código.
+
+As condições com apenas um tipo de bloco, é chamada de condição simples.
+Ja a condição com dois tipos de blocos, são chamadas de condição composta.
+*/
+
