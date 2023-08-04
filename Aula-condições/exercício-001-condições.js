dd0d0b63fc4b5833278f70f2d637a08367cbec11
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-04 21:26:45 +0000

    exercício 001

--- Aula-condições/exercício-001-condições.js
+++ Aula-condições/exercício-001-condições.js
@@ -6,3 +6,9 @@ As condições com apenas um tipo de bloco, é chamada de condição simples.
 Ja a condição com dois tipos de blocos, são chamadas de condição composta.
 */
 
+var velocidade = 65;
+if (velocidade > 60) { //Quando se utiliza apenas o if, temos uma condição simples.
+    console.log(`A velocidade do seu carro é ${velocidade}km/h. Multado!`);
+} else{// Utilizando if e else, temos uma condição composta. 
+    console.log(`A velocidade do seu carro é ${velocidade}km/h. Dirija com cuidado!`);
+}
