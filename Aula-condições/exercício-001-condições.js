6995f150dbf67eefd142b852abb2f0fd7a6dc641
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-04 22:04:16 +0000

    exercícios

--- Aula-condições/exercício-001-condições.js
+++ Aula-condições/exercício-001-condições.js
@@ -6,6 +6,9 @@ As condições com apenas um tipo de bloco, é chamada de condição simples.
 Ja a condição com dois tipos de blocos, são chamadas de condição composta.
 */
 
+const entrada= require('readline-sync');
+
+
 var velocidade = 65;
 if (velocidade > 60) { //Quando se utiliza apenas o if, temos uma condição simples.
     console.log(`A velocidade do seu carro é ${velocidade}km/h. Multado!`);
@@ -19,4 +22,31 @@ if (pais == "Brasil" || pais == "brasil") {
     console.log("Você é brasileiro.");
 }else{
     console.log("Você é estrangeiro.");
+}
+
+//Verificando se um número é positivo. 
+
+var positivo = entrada.question("Digite um número inteiro: ")
+if (positivo >= 0) {
+    console.log("Este número é positivo.");
+} else {
+    console.log("Este número é negativo.");
+} 
+
+//Verificando se um número é par 
+
+var par = entrada.question("Digite um número: ")
+if (par % 2 == 0) {
+    console.log("Este número é par.");
+} else {
+    console.log("Este número é impar.");
+}
+
+//Verificando se uma string é vazia
+
+var string = entrada.question("Digite uma palavra: ")
+if (string.length === 0) { //Para verificar uma string vazia usei o .length que conta os caracteres.
+    console.log("String vazia.");
+} else{
+    console.log(`A string digitada foi ${string}.`);
 }
