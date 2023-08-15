d04ca72640ac5211a87d3c648c8853486784ce09
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-15 13:00:55 +0000

    exercício 001

--- Aula-funções/exercício-001.js
+++ Aula-funções/exercício-001.js
@@ -1,3 +1,22 @@
-const entrada= require('readline-sync');
+const entrada = require('readline-sync');
 
 
+/* Funções. 
+    As funções são execultadas assim que são chamadas ou em decorrência de um evento.
+    Um função pode receber parâmetos e retornar um resultado. 
+    Todas as funções podem ter chamada, parêmetro(s), ação(os) e retorno.
+
+    Estrutura de código de uma função: function ação(parametros){}
+*/
+
+    exemplo: 
+       function parimp(res) {
+            if (res % 2 == 0) {
+                return "par"
+            } else {
+                return "impar"
+            }
+        }
+    let res = entrada.question("Digite um número: ")
+    
+    console.log(parimp(res));
