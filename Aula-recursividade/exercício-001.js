f6da0bab05862be976b8bbcf1fe68e8e3e1febe7
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-15 18:41:05 +0000

    exercício 001

--- Aula-recursividade/exercício-001.js
+++ Aula-recursividade/exercício-001.js
@@ -1,11 +1,32 @@
 const entrada = require('readline-sync'); 
 
-function fatorial(n) {
+/* function fatorial(n) {
     if (n === 0 || n === 1) {
         return 1; 
     } else {
-        return n * fatorial(n-1); 
+        return numero * fatorial(numero-1); 
     }
 }
-const numero = 5; 
-console.log(`O fatorial de ${numero} é ${fatorial(numero)}`);
+const numero = entrada.question('Digite um número: '); 
+console.log(`O fatorial de ${numero} é ${fatorial(numero)}`); */
+
+
+//Exercício função recusrsiva
+
+/* function somaRecursiva(n){
+    if (n === 1) {
+        return 1; 
+    } else {
+        return n + somaRecursiva(n - 1); 
+    }
+}
+console.log(somaRecursiva(5)); */
+
+function fibonacci(n){
+    if (n <= 1) {
+        return n; 
+    } else {
+        return fibonacci(-1) + fibonacci(-2); 
+    }
+}
+console.log(fibonacci(5));
