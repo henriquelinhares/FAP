28bef48bffee45c46517c7cd4f7c3b5b5e71f396
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-15 14:15:27 +0000

    exercícios 001, 002

---
+++ Aula-funções/exercício-002.js
@@ -0,0 +1,47 @@
+const entrada = require('readline-sync');
+
+//Exercício de funções com retorno
+
+
+//Criando uma função para somar. 
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+let n1 = Number(entrada.question('Digite um número: '));
+let n2 = Number(entrada.question('Digite outro número: '));
+
+console.log(soma(n1, n2));
+
+
+//Criando uma função para verificar número par ou impar.
+
+function ehPar(n) {
+    if (n % 2 == 0) {
+        return "Este número é par!"
+    } else {
+        return "Este número é impar!"
+    }
+}
+let n = entrada.question("Digite um número: ")
+console.log(ehPar(n));
+
+
+//Criando uma função que verificar qual o maior número
+
+function maiorNumero(numero1, numero2, numero3) {
+    if (numero1 > numero2 && numero1 > numero3) {
+        return numero1 + " é o maior número."
+    } else if(numero2 > numero1 && numero2 > numero3){
+        return numero2 + " é o maior número."
+    } else if(numero3 > numero1 && numero3 > numero2){
+        return numero3 + " é o maior número."
+    } else{
+        return "resposta inválida."
+    }
+} 
+let numero1 = entrada.question("Digite algum número: ");
+let numero2 = entrada.question('Digite outro número: ');
+let numero3 = entrada.question('Digite um terceiro número: ');
+
+console.log(maiorNumero(numero1, numero2, numero3));
