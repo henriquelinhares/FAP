ddaa90751d7dfa46a9b28b4d82e040ffcbd73201
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-17 17:23:59 +0000

    exercício 001

---
+++ Aula-Exceções/exercício-001.js
@@ -0,0 +1,8 @@
+const entrada = require('readline-sync'); 
+
+/* Error: É a interrupção involuntária na execução do código, normalmente como exceções.
+Erros de sintaxe são um tipo de exceção que ocorre quando a sintaxe está incorreta. 
+
+throwing an exception: Envia uma mensagem de que algo errado na execução pretendida do codigo,
+pelo javascript ou pela pessoa desenvolvida com a palavra-chave throw. 
+*/
