0237e8327ee7360f743e6a15f77924a15ca5e1d1
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-17 17:37:18 +0000

    exercício 001

--- Aula-Exceções/exercício-001.js
+++ Aula-Exceções/exercício-001.js
@@ -5,4 +5,28 @@ Erros de sintaxe são um tipo de exceção que ocorre quando a sintaxe está inc
 
 throwing an exception: Envia uma mensagem de que algo errado na execução pretendida do codigo,
 pelo javascript ou pela pessoa desenvolvida com a palavra-chave throw. 
+
+Palavras chave: Try, Catch, Finally. 
+
+    Try: Bloco de código que pode lançar uma exceção.
+    Catch: Bloco de código que será executado se uma exceção for lançada.
+    Finally: Parte opcional do código que será executado após o bloco try ou após o bloco catch.
 */
+
+// Sintaxe das exceções: 
+
+    try {
+
+    } catch(error){
+
+    } finally{
+        
+    }
+
+
+
+
+Exemplo: 
+    throw 'minha-excecao'; // saída: Uncaught 'minha excecao'
+    throw true; // saída: Uncaught true
+    throw 42; // saída: Uncaught 42
