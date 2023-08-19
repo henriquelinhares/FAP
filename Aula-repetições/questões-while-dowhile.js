42261f3071f3a4e1c46a513d4a0b6352413e4387
Henrique Linhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-19 19:15:10 +0000

    questões while

---
+++ Aula-repetições/questões-while-dowhile.js
@@ -0,0 +1,16 @@
+const entrada = require('readline-sync');
+
+//Crie um laço while que irá iterar enquanto n for menor ou igual a 5.
+
+let n = Number(entrada.question('Digite um número inteiro: '));
+while (n <= 5) {
+    console.log('Passo ' + n);
+    n++
+}
+
+//Criando o vetor e acessando as informações 
+
+let nota = entrada.question('Digite uma nota entre 0 e 10: ');
+while (nota > 10) {
+    console.log('Valor inválido. Digite novamente.');
+}
