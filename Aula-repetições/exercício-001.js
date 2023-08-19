6cd9b433589324322f79bf796fecfef577a8383b
Henrique Linhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-19 18:33:06 +0000

    exercício 001

--- Aula-repetições/exercício-001.js
+++ Aula-repetições/exercício-001.js
@@ -6,6 +6,10 @@ const entrada = require('readline-sync');
 
    while: Podemos utilizar a estrutura de repetição while caso seja necessário repetir um bloco de comandos por N vezes.
    Sempre que tivemos a palavra while, devemos ler como enquanto. while = enquanto.
+
+   Junto ao while, também temos o do-while. O do-while tem o mesmo conceito que o while com uma única diferença, 
+   a condição é verificada após os comandos do bloco serem executados, ou seja, mesmo que a condição seja falsa, é garantia 
+   que o bloco será executado ao menos uma vez. 
 */
 
 //Exemplo while: 
@@ -16,3 +20,11 @@ while(i < 11){
     console.log('5 x ' + i + ' = ' + 5*i);
     i++; // Aqui a váriavel i é igual a i = i + 1. 
 }
+
+//Exemplo do-while: 
+let contador = 0;
+
+do{
+	console.log("O contador vale: " + contador);
+	contador++;
+}while(contador == 1)
