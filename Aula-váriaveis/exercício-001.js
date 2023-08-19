1dcd5fca3cb8c1df6af3888e000020de3dfeb264
henriquelinhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-19 13:12:30 +0000

    exercício 001

---
+++ Aula-váriaveis/exercício-001.js
@@ -0,0 +1,21 @@
+//As variaveis servem para guardar dados
+
+/*Variaveis do tipo primitivo podem ser: 
+    tipo número que pode ser número inteiro ou número real/pontoflutuante
+    tipo string/palavas ou números contendo pontos ou traços
+    tipo boolean que vai ser true/verdadeiro ou false/falso
+*/
+
+// Existe um comando chamado typeof que serve para identificar o tipo da váriavel. 
+
+var n1 = 5; //variavel do tipo numero inteiro
+var n2 = 10.5; //variavel do tipo numero real/ponto flutuante
+var n3 = "155.567.876-45"; //variavel do tipo string, pois além de numeros contem ponto e traço. CPF, RG e etc.
+
+console.log(n1 + n2 + " é o valor que o cpf " + n3 + " possui em sua conta.");
+
+var s1 = "JavaScript"; //variavel do tipo string
+var s2 = "Luiz";
+var s3 = "Henrique";
+
+console.log("O aluno " + s2 + " estuda a linguagem " + s1); //comando para imprimir as variaveis 
