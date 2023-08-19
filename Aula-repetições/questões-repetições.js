4d74544e329cec7b353c6f17ccc9c7753bb9fbe3
Henrique Linhares <124594651+henriquelinhares@users.noreply.github.com>
2023-08-19 22:01:10 +0000

    questões repetições

--- Aula-repetições/questões-repetições.js
+++ Aula-repetições/questões-repetições.js
@@ -1,5 +1,7 @@
 const entrada = require('readline-sync');
 
+//https://www.javascriptprogressivo.net/2018/12/Lista-Exercicios-Lacos-Loopings-WHILE-FOR.html
+
 /* 1ª questão. Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até 
 que o usuário informe um valor válido.
 */
@@ -24,6 +26,19 @@ while (nomeUsuario === senha) {
     nomeUsuario = entrada.question('Digite seu nome de usuário: ').toUpperCase();
     senha = entrada.question('Digite sua senha: ').toUpperCase();
 } console.log("Login realizado. Obrigado.");
+
+
+/* 3ª questão.
+ Faça um programa que leia e valide as seguintes informações:
+Nome: maior que 3 caracteres;
+Idade: entre 0 e 150;
+Salário: maior que zero;
+Sexo: 'f' ou 'm';
+Estado Civil: 's', 'c', 'v', 'd';
+Dica: se sua variável é texto, o tamanho dela está armazenado em: texto.length
+*/
+
+
 
 
 
