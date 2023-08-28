0dd0aa045ecfc8618f4d4ac1ac9f4298b1139283
Henrique Linhares <linhares.luiz8@gmail.com>
2023-08-28 12:02:46 -0300

    variaveis compostas

--- Aula-variaveis-compostas/exercício-001.js
+++ Aula-variaveis-compostas/exercício-001.js
@@ -21,14 +21,28 @@ Para saber o comprimeto de um váriavel, basta utilizar o comando/atributo lengt
         A.length
 
 Para colocar todos os elementos de uma variavel em ordem crescente, utilizamos o comando sort: variavel.sort()
-    A.sort()
+    let A = [5,8,4,6,7]
+        A.sort()
+        A = [4,5,6,7,8]
+
+Para imprimir o valor de todos os vetores na tela,  um de cada vez, sem precisar criar vários console.log, utilizamos a estrutura de repetição for:
+    let A = [5,8,4,6,7]
+    for(let pos = 0; pos < A.length; pos++){console.log(A[pos])}; 
+    
+    vejamos que foi criado uma variavel chamada pos, ela funciona como um contador. Em seguida a condição foi caso pos seja menor que o tamanho da variavel A, a estrutura repete a impressão da variavel pos acrescentando mais 1. Ficar atendo que, no console.log a variavel que for ser impressa deverá conter entre chaves a variavel contador. 
 */
 
-let num = [5, 8, 2, 9, 3]
+/* let num = [5, 8, 2, 9, 3]
     num[5] = 4;
     num.push(7);
 
 console.log(`Nosso vetor é o: ${num}`);
 console.log(`Nosso vetor em ordem é o: ${num.sort()}`);
 console.log(`O tamanho da nossa variavel é: ${num.length}`);
-console.log(`O nosso elemnto na segunda posição é 0: ${num[2]}`);
+console.log(`O nosso elemnto na segunda posição é 0: ${num[2]}`); */
+
+let A = [5,8,4,6,7]
+    A.sort()
+for(let pos = 0; pos < A.length; pos++){console.log(A[pos])}; 
+
+
