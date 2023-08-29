1dbf5b5f1efc6e2a96f71e48610c270c1a2d3074
Henrique Linhares <linhares.luiz8@gmail.com>
2023-08-29 00:25:49 -0300

    arquivos

--- arquivo.js
+++ arquivo.js
@@ -11,18 +11,38 @@ Quando a votação for finalizada, o código deverá mostrar o vencedor, aquele 
 
 const entrada= require('readline-sync');
 
-let voto = entrada.question('Fulano: 1; Sicrano: 2; Beltrano: 3; Branco: 0. Digite o seu voto: ');
-while (voto < 0 || voto > 3 || isNaN(voto)) {
+let fulano = 0;
+let sicrano = 0;
+let beltrano = 0;
+let nulo = 0;
+let branco = 0;
+
+let voto = Number(entrada.question('Veja o candidato e escolha um. candidato Fulano: 1; candidato Sicrano: 2; Candidato Beltrano: 3; Em Branco: 4; Para encerrar a votacao: 5. Digite o seu voto: '));
+
+while (isNaN(voto)) {
     console.log("Você escolheu uma opção inválida.");
-    voto = entrada.question('Fulano: 1; Sicrano: 2; Beltrano: 3; Branco: 0. Digite o seu voto: ');
+    voto = Number(entrada.question('Veja o candidato e escolha um. candidato Fulano: 1; candidato Sicrano: 2; Candidato Beltrano: 3; Em Branco: 4; Para encerrar a votacao: 5. Digite o seu voto: '));
 } 
-
-function eleição(fulano, sicrano, beltrano, branco) {
-    let candidatos = {
-    fulano: 889,
-    sicrano: 847,
-    beltrano: 515,
-    branco: 0
-    }
-
+while (voto != 5) {
+    if (voto == 1) {
+        fulano = fulano + 1
+    } else if (voto ==2 ){
+        sicrano = sicrano + 1
+    } else if(voto == 3){
+        beltrano = beltrano + 1
+    } else if (voto == 4) {
+        branco = branco + 1
+    } else if(voto != 1 || voto != 2 || voto != 3 || voto != 4 || voto != 5){
+        nulo = nulo + 1
+    } 
+    voto = Number(entrada.question('Veja o candidato e escolha um. candidato Fulano: 1; candidato Sicrano: 2; Candidato Beltrano: 3; Em Branco: 4; Para encerrar a votacao: 5. Digite o seu voto: '));
 }
+if(voto == 5){
+    console.log('Votação encerrada.');
+    function getMaxOfArray(numArray) {
+        return Math.max.apply(null, numArray);
+    }
+    console.log("O vencedor teve: " + getMaxOfArray([fulano, beltrano, sicrano, branco, nulo]) + ' votos.');
+}
+
+
