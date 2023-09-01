b5a285532b20d807b5e7af86740a6b583eb87655
Henrique Linhares <linhares.luiz8@gmail.com>
2023-09-01 16:05:57 -0300

    eoperadores

---
+++ Aula-condições/exercício-operadores-relacionais-logicos.js
@@ -0,0 +1,45 @@
+const entrada= require('readline-sync'); 
+
+/* Operadores relacionais.
+Operador	             Descrição	                                      Exemplo
+==	                     Igual	                                         (A == B) é false
+===	              Igual(tanto o valor quanto o tipo de objeto)	         (A === B) é false
+!=	                     Não igual	                                     (A != B) é true
+>	                     Maior que	                                     (A > B) é false
+<	                     Menor que	                                     (A < B) é true
+>=	                     Maior ou igual que	                             (A >= B) é false
+<=	                     Menor ou igual que	                             (A <= B) é true
+
+*/
+
+/* Operadores lógicos.
+Operador	         Descrição	             Exemplo
+&&	               Operador AND (E)	      (A && B) é false
+||	               Operador OR (OU)	      (A || B) é true
+!	      Operador NOT (NÃO) ou Negação   !(A && B) é true
+ 
+*/
+
+// let nome = entrada.question('Digite o seu nome: ');
+// let endereço = entrada.question('Digite o seu endereço: ');
+
+// while (!endereço) {
+//     console.log('Campo em branco. Por favor, digite o seu endereço.');
+//     endereço = entrada.question('Digite o seu endereço: ');
+// }
+
+// console.log(`O senhor ${nome} mora na ${endereço}.`); 
+
+// function convert(x) {
+//     switch(typeof x) {
+//     case 'number': // Converte o número para um inteiro hexadecimal
+//     return x.toString(16);
+//     case 'string': // Retorna a string colocada entre apóstrofos
+//     return '"' + x + '"';
+//     default: // Converte qualquer outro tipo da maneira usual
+//     return String(x);
+//     }
+//     }
+
+// console.log(convert(79)); 
+
---
+++ arquivo-2.js
@@ -0,0 +1,11 @@
+// Exercítando questões de variaveis
+
+// let fistName = 'Luiz'
+// let lastName = 'Linhares'
+
+// let address = 'Rua Belo Jardim'
+//     address = 'Rua da baixaria'
+
+// console.log(`O senhor ${fistName} ${lastName} mora na ${address}.`);
+
+
