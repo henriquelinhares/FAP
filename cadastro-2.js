e8316725ab5802c2f622fdfde2bc3be870790c23
Henrique Linhares <linhares.luiz8@gmail.com>
2023-09-11 22:04:42 -0300

    cadastro

---
+++ cadastro-2.js
@@ -0,0 +1,9 @@
+const entrada = require('readline-sync');
+
+console.log('===============MENU==============');
+console.log('0: para sair.');
+console.log('1: Acessar dados dos funcionarios.');
+console.log('2: Verificar matricula dos funcionarios.');
+
+let funcionario
+
